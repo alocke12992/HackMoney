e15e008008d180560628d2ab8d5bc92fe6ab602b
Melissa Hill <mhill2040@gmail.com>
2018-03-24 15:05:12 -0600

    seed file

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,253 +1,255 @@
 Item = [ {
 
-  name: "Combonation Plate 1"
-  description: "One Bean Tostada and One Beef Taco"
-  price: 7.25
+  name: "Combonation Plate 1",
+  description: "One Bean Tostada and One Beef Taco",
+  price: "7.25",
   Image: "http://losfavs.com/wp-content/uploads/2017/10/Combo2.png"
   },
 
-  {name: "Combonation Plate 2"
-  description: "Two Beef Tacos with Cheese and Lettuce"
-  price: 7.25
+  {name: "Combonation Plate 2",
+  description: "Two Beef Tacos with Cheese and Lettuce",
+  price: "7.25",
   Image: "http://texasmargaritas.net/files/58545688.jpg"
   },
 
-  {name: "Combonation Plate 3"
-  description: "Two Cheese Enchelidas with Lettuce"
-  price: 7.25
+  {name: "Combonation Plate 3",
+  description: "Two Cheese Enchelidas with Lettuce",
+  price: "7.25",
   Image: "https://habanerostacogrill.com/wp-content/uploads/2017/02/2-2-CHEESE-ENCHILADAS-PLATE.jpg"
   },
 
-  {name: "Combonation Plate 4"
-  description: "Two Carne Asada Tacos (Corn Tortillas with Steak Meat, Pico de Gallo & Guacamole)"
-  price: 8.45
+  {name: "Combonation Plate 4",
+  description: "Two Carne Asada Tacos (Corn Tortillas with Steak Meat, Pico de Gallo & Guacamole)",
+  price: "8.45",
   Image: "http://www.sombreritosfood.com/img/Tacos.jpg"
   },
 
-  {name: "Combonation Plate 5"
-  description: "Two Beef Burritos"
-  price: 7.25
+  {name: "Combonation Plate 5",
+  description: "Two Beef Burritos",
+  price: "7.25",
   Image: "http://robertostacoshop.com/wp-content/uploads/2017/10/Combo05.jpg"
   },
 
-  {name: "Burritos"
-  description: "Breakfast (Three pieces of ham, two eggs and cheese)"
-  price: 4.65
+  {name: "Burritos",
+  description: "Breakfast (Three pieces of ham, two eggs and cheese)",
+  price: "4.65",
   Image: "https://food.fnr.sndimg.com/content/dam/images/food/fullset/2013/2/14/0/FNK_breakfast-burrito_s4x3.jpg.rend.hgtvcom.616.462.suffix/1382542427230.jpeg"
   },
 
-  {name: "Burritos"
-  description: "Bean and Cheese"
-  price: 3.35
+  {name: "Burritos",
+  description: "Bean and Cheese",
+  price: "3.35",
   Image: "https://images-gmi-pmc.edge-generalmills.com/074a3680-3adc-4aae-85f5-1e3a4f2caa34.jpg"
   },
 
-  {name: "Burritos"
-  description: "Chicken (Cooked in a spicy red sauce)"
-  price: 4.65
+  {name: "Burritos",
+  description: "Chicken (Cooked in a spicy red sauce)",
+  price: "4.65",
   Image: "https://d1doqjmisr497k.cloudfront.net/-/media/mccormick-us/recipes/zatarains/r/800/red-bean-chicken-and-cheese-burritos.ashx?vd=20171018T135024Z&hash=0B060FD4FF1BF5491B8A9A52840587B64C62AA4D"
   },
 
-  {name: "Burritos"
-  description: "Chorizo (Mexican sausage with 2 eggs, cheese and potatoes)"
-  price: 4.80
+  {name: "Burritos",
+  description: "Chorizo (Mexican sausage with 2 eggs, cheese and potatoes)",
+  price: "4.80",
   Image: "https://i.ytimg.com/vi/b3Ie-p84zNc/hqdefault.jpg"
   },
 
-  {name: "Burritos"
-  description: "Beef (Cooked with bell pepper, onion, tomato and yellow chile with cheese and beans)"
-  price: 4.65
+  {name: "Burritos",
+  description: "Beef (Cooked with bell pepper, onion, tomato and yellow chile with cheese and beans)",
+  price: "4.65",
   Image: "http://assets.kraftfoods.com/recipe_images/opendeploy/107771_640x428.jpg"
   },
 
-  {name: "Enchiladas"
-  description: "Beef (Cooked with bell pepper, onion, tomato, yellow chile, with lettuce)"
-  price: 5.05
+  {name: "Enchiladas",
+  description: "Beef (Cooked with bell pepper, onion, tomato, yellow chile, with lettuce)",
+  price: "5.05",
   Image: "https://d1doqjmisr497k.cloudfront.net/-/media/mccormick-us/recipes/mccormick/b/800/beef-enchiladas.ashx?vd=20171018T134025Z&hash=739AF040A2F3E0DF060F34B207B6344921E1ADCA"
   },
 
-  {name: "Enchiladas"
-  description: "Chicken (Cooked in a spicy red sauce, with lettuce)"
-  price: 5.05
+  {name: "Enchiladas",
+  description: "Chicken (Cooked in a spicy red sauce, with lettuce)",
+  price: "5.05",
   Image: "https://www.cscassets.com/recipes/wide_cknew/wide_26152.jpg"
   },
 
-  {name: "Enchiladas"
-  description:"Cheese (with lettuce)"
-  price: 5.05
+  {name: "Enchiladas",
+  description:"Cheese (with lettuce)",
+  price: "5.05",
   Image: "https://images-gmi-pmc.edge-generalmills.com/2b23cf23-2d3b-48a3-93b1-912004fbe8d0.jpg"
   },
 
-  {name: "Tacos"
-  description: "Shredded Beef Taco (with cheese and lettuce deep fried)"
-  price: 2.65
+  {name: "Tacos",
+  description: "Shredded Beef Taco (with cheese and lettuce deep fried)",
+  price: "2.65",
   Image: "https://www.pepperscale.com/wp-content/uploads/2016/02/Spicy-Beef-Tacos-e1456678395359.jpeg"
   },
 
-  {name: "Tacos"
-  description: "Chicken Taco (Cooked in spicy red sauce)"
-  price: 2.50
+  {name: "Tacos",
+  description: "Chicken Taco (Cooked in spicy red sauce)",
+  price: "2.50",
   Image: "https://food.fnr.sndimg.com/content/dam/images/food/fullset/2017/3/24/0/KC1304H_Spicy-Chicken-Tacos_s4x3.jpg.rend.hgtvcom.616.462.suffix/1490378555671.jpeg"
   },
 
-  {name: "Tacos"
-  description: "Care Asada Taco (steak meat with guacamole and pico de gallo)"
-  price: 3.85
+  {name: "Tacos",
+  description: "Care Asada Taco (steak meat with guacamole and pico de gallo)",
+  price: "3.85",
   Image: "https://food.fnr.sndimg.com/content/dam/images/food/fullset/2011/7/26/0/FO1D34_tacos-carne-asada_s4x3.jpg.rend.hgtvcom.616.462.suffix/1384541146262.jpeg"
   },
 
-  {name: "Tacos"
-  description: "Three rolled tacos (Taquitos) with cheese and guacamole"
-  price: 3.71
-  Image: https://filibertos.com/wp-content/uploads/2015/08/DSC_0039-570x350.jpg
+  {name: "Tacos",
+  description: "Three rolled tacos (Taquitos) with cheese and guacamole",
+  price: "3.71",
+  Image: "https://filibertos.com/wp-content/uploads/2015/08/DSC_0039-570x350.jpg"
   },
 
-  {name: "Tacos"
-  description: "Fish Taco (with tarter sauce, pico de gallo and cabbage)"
-  price: 3.65
-  Image: https://toriavey.com/images/2012/07/Crispy-Fish-Tacos.jpg
+  {name: "Tacos",
+  description: "Fish Taco (with tarter sauce, pico de gallo and cabbage)",
+  price: "3.65",
+  Image: "https://toriavey.com/images/2012/07/Crispy-Fish-Tacos.jpg"
   },
 
-  {name: "Tostadas"
-  description: "Beans (with cheese, tomato and lettuce)"
-  price: 3.00
-  Image: http://3.bp.blogspot.com/--lu5-1JkXNw/T3eixobPl8I/AAAAAAAAAWg/Zp3o8Txdbq8/s1600/beantostada.jpg
+  {name: "Tostadas",
+  description: "Beans (with cheese, tomato and lettuce)",
+  price: "3.00",
+  Image: "http://3.bp.blogspot.com/--lu5-1JkXNw/T3eixobPl8I/AAAAAAAAAWg/Zp3o8Txdbq8/s1600/beantostada.jpg"
   },
 
-  {name: "Tostads"
-  description: "Beef (cooked with bell pepper, onion, tomato, and yellow hile, with beans, cheese and lettuce)"
-  price: 3.40
-  Image: http://blogchef.net/wp-content/uploads/2011/06/beef_tostadas_1.jpg
+  {name: "Tostads",
+  description: "Beef (cooked with bell pepper, onion, tomato, and yellow hile, with beans, cheese and lettuce)",
+  price: "3.40",
+  Image: "http://blogchef.net/wp-content/uploads/2011/06/beef_tostadas_1.jpg"
   },
 
-  {name: "Tostadas"
-  description: "Chicken (cooked in a spicy red sauce, with beans, cheese and lettuce)"
-  price: 3.40
-  Image: http://partaste.com/worldrecipes/wp-content/uploads/sites/2/2014/12/tostada_de_tinga_de_pollo_mexican_food_partaste.jpg
+  {name: "Tostadas",
+  description: "Chicken (cooked in a spicy red sauce, with beans, cheese and lettuce)",
+  price: "3.40",
+  Image: "http://partaste.com/worldrecipes/wp-content/uploads/sites/2/2014/12/tostada_de_tinga_de_pollo_mexican_food_partaste.jpg"
   },
 
-  {name: "Tortas"
-  description: "Ham (with guacamole and lettuce)"
-  price: 4.50
-  Image: http://thelatinkitchen.com/sites/default/files/styles/recipe-big/public/main/233/torta-ham-and-cheese_main.jpg?itok=7Ran_XdD
+  {name: "Tortas",
+  description: "Ham (with guacamole and lettuce)",
+  price: "4.50",
+  Image: "http://thelatinkitchen.com/sites/default/files/styles/recipe-big/public/main/233/torta-ham-and-cheese_main.jpg?itok=7Ran_XdD"
   },
 
-  {name: "Tortas"
-  description: "Beef (cooked with bell pepper, onion, tomato, yellow chile, with guacamole and lettuce)"
-  price: 4.50
-  Image: http://1.bp.blogspot.com/--GXLgIZkzAg/UJXkbtSercI/AAAAAAAAAF4/MHc1eEIfH54/s1600/IMG_2100.800x533.113.6K.jpg
+  {name: "Tortas",
+  description: "Beef (cooked with bell pepper, onion, tomato, yellow chile, with guacamole and lettuce)",
+  price: "4.50",
+  Image: "http://1.bp.blogspot.com/--GXLgIZkzAg/UJXkbtSercI/AAAAAAAAAF4/MHc1eEIfH54/s1600/IMG_2100.800x533.113.6K.jpg"
   },
 
-  {name: "Tortas"
-  description: "Chicken (cooked in a spicy red sauce, with guacamole and lettuce)"
-  price: 4.50
-  Image: http://www.averagebetty.com/images_2012/grilled_chicken_torta_3_580.jpg
+  {name: "Tortas",
+  description: "Chicken (cooked in a spicy red sauce, with guacamole and lettuce)",
+  price: "4.50",
+  Image: "http://www.averagebetty.com/images_2012/grilled_chicken_torta_3_580.jpg"
   },
 
-  {name: "Tortas"
-  description: "Chorizo (mexican sausage with one egg, guacamole and lettuce)"
-  price: 4.50
-  Image: https://s3.amazonaws.com/static.realcaliforniamilk.com/media/recipes_5-5-17/PotatoChorizoEggTorta.jpg
+  {name: "Tortas",
+  description: "Chorizo (mexican sausage with one egg, guacamole and lettuce)",
+  price: "4.50",
+  Image: "https://s3.amazonaws.com/static.realcaliforniamilk.com/media/recipes_5-5-17/PotatoChorizoEggTorta.jpg"
   },
 
-  {name: "Tortas"
-  description: "Carne Asada (steak meat with guacamole and lettuce)"
-  price: 4.90
-  Image: https://www.seriouseats.com/images/2015/04/20150430-tortas-in-san-francisco-5.jpg
+  {name: "Tortas",
+  description: "Carne Asada (steak meat with guacamole and lettuce)",
+  price: "4.90",
+  Image: "https://www.seriouseats.com/images/2015/04/20150430-tortas-in-san-francisco-5.jpg"
   },
 
-  {name: "Side Orders"
-  description: "1/2 Pt Rice"
-  price: 2.00
-  Image: https://i0.wp.com/www.honestandtruly.com/wp-content/uploads/2017/01/Easy-peasy-Spanish-Rice-in-the-Instant-Pot.jpg?resize=700%2C550&ssl=1
+  {name: "Side Orders",
+  description: "1/2 Pt Rice",
+  price: "2.00",
+  Image: "https://i0.wp.com/www.honestandtruly.com/wp-content/uploads/2017/01/Easy-peasy-Spanish-Rice-in-the-Instant-Pot.jpg?resize=700%2C550&ssl=1"
   },
 
-  {name: "Side Orders"
-  description: "1/2 Pt Beans"
-  price: 2.00
-  Image: https://smhttp-ssl-26410.nexcesscdn.net/media/catalog/product/cache/2/image/9df78eab33525d08d6e5fb8d27136e95/r/e/refried-bean-flakes-honeyville-9.jpg
+  {name: "Side Orders",
+  description: "1/2 Pt Beans",
+  price: "2.00",
+  Image: "https://smhttp-ssl-26410.nexcesscdn.net/media/catalog/product/cache/2/image/9df78eab33525d08d6e5fb8d27136e95/r/e/refried-bean-flakes-honeyville-9.jpg"
   },
 
-  {name: "Side Orders"
-  description: "1/2 Pt Guacamole"
-  price: 3.80
-  Image: //fthmb.tqn.com/lEYYfeH7ssWp_C-1gH7tuPxLFdM=/960x0/filters:no_upscale()/guacamole-56b0af4b5f9b58b7d024dd41.jpg
+  {name: "Side Orders",
+  description: "1/2 Pt Guacamole",
+  price: "3.80",
+  Image: "https://www.rubios.com/sites/default/files/menu/side-guacamole.jpg"
   },
 
-  {name: "Side Orders"
-  description: "1/2 pt Pico de Gallo"
-  price: 2.15
-  Image: https://www.seriouseats.com/recipes/images/2013/06/20130621-pico-de-gallo-13.jpg
+  {name: "Side Orders",
+  description: "1/2 pt Pico de Gallo",
+  price: "2.15",
+  Image: "https://www.seriouseats.com/recipes/images/2013/06/20130621-pico-de-gallo-13.jpg"
   },
 
-  {name: "Side Orders"
-  description: "Quesadilla (flour tortilla with cheese one per order)"
-  price: 3.30
-  Image: https://www.tacobueno.com/assets/food/quesadilla/Quesadilla_Cheese_490x360.jpg?v=1
+  {name: "Side Orders",
+  description: "Quesadilla (flour tortilla with cheese one per order)",
+  price: "3.30",
+  Image: "https://www.tacobueno.com/assets/food/quesadilla/Quesadilla_Cheese_490x360.jpg?v=1"
   },
 
-  {name: "Extras"
-  description: "1/4 Pt. Shredded Cheese"
-  price: .95
-  Image: https://images.eatthismuch.com/site_media/img/501037_hpsillymom_4682b33b-5577-4baa-8d3d-42352d179f68.png
+  {name: "Extras",
+  description: "1/4 Pt. Shredded Cheese",
+  price: ".95",
+  Image: "https://images.eatthismuch.com/site_media/img/501037_hpsillymom_4682b33b-5577-4baa-8d3d-42352d179f68.png"
   },
 
-  {name: "Extras"
-  description: "1/4 Pt. Guacamole"
-  price: 1.90
-  Image: https://fthmb.tqn.com/lEYYfeH7ssWp_C-1gH7tuPxLFdM=/960x0/filters:no_upscale()/guacamole-56b0af4b5f9b58b7d024dd41.jpg
+  {name: "Extras",
+  description: "1/4 Pt. Guacamole",
+  price: "1.90",
+  Image: "https://www.rubios.com/sites/default/files/menu/side-guacamole.jpg"
   },
 
-  {name: "Extras"
-  description: "1/4 Pt. Sour cream"
-  price: .95
-  Image: https://www.copykat.com/wp-content/uploads/2010/12/sour-cream.jpg
+  {name: "Extras",
+  description: "1/4 Pt. Sour cream",
+  price: ".95",
+  Image: "https://www.copykat.com/wp-content/uploads/2010/12/sour-cream.jpg"
   },
 
-  {name: "Extras"
-  description: "1/4 Pt. Pico de Gallo"
-  price: .95
-  Image: https://www.seriouseats.com/recipes/images/2013/06/20130621-pico-de-gallo-13.jpg
+  {name: "Extras",
+  description: "1/4 Pt. Pico de Gallo",
+  price: ".95",
+  Image: "https://www.seriouseats.com/recipes/images/2013/06/20130621-pico-de-gallo-13.jpg"
   },
 
-  {name: "Extras"
-  description: "Three Corn Tortillas"
-  price: 1.45
-  Image: https://fthmb.tqn.com/_QqymGtKsBWwXqcTlQb45X29iAg=/960x0/filters:no_upscale()/corn-tortilla-stack-g18-56a8c1e15f9b58b7d0f4da37.jpg
+  {name: "Extras",
+  description: "Three Corn Tortillas",
+  price: "1.45",
+  Image: "https://fthmb.tqn.com/_QqymGtKsBWwXqcTlQb45X29iAg=/960x0/filters:no_upscale()/corn-tortilla-stack-g18-56a8c1e15f9b58b7d0f4da37.jpg"
   },
 
-  {name: "Beverages / Fresh Chilled Tamarindo, Horchata, Jamaica & Limonada"
-  description: "Small 16 oz"
-  price: 2.00
-  Image: https://s3-media4.fl.yelpcdn.com/bphoto/BfLofBDabsQXE4DRj7n_Rg/o.jpg
-
-  {name: "Beverages / Fresh Chilled Tamarindo, Horchata, Jamaica & Limonada"
-  description: "Medium 24 oz"
-  price: 2.25
-  Image: https://s3-media4.fl.yelpcdn.com/bphoto/BfLofBDabsQXE4DRj7n_Rg/o.jpg
+  {name: "Beverages / Fresh Chilled Tamarindo, Horchata, Jamaica & Limonada",
+  description: "Small 16 oz",
+  price: "2.00",
+  Image: "https://s3-media4.fl.yelpcdn.com/bphoto/BfLofBDabsQXE4DRj7n_Rg/o.jpg"
   },
 
-  {name: "Beverages / Fresh Chilled Tamarindo, Horchata, Jamaica & Limonada"
-  description: "Large 32 oz"
-  price: 2.60
-  Image: https://s3-media4.fl.yelpcdn.com/bphoto/BfLofBDabsQXE4DRj7n_Rg/o.jpg
+  {name: "Beverages / Fresh Chilled Tamarindo, Horchata, Jamaica & Limonada",
+  description: "Medium 24 oz",
+  price: "2.25",
+  Image: "https://s3-media4.fl.yelpcdn.com/bphoto/BfLofBDabsQXE4DRj7n_Rg/o.jpg"
   },
 
-  {name: "Carboneted Drinks / Coca Cola, Diet Coke, Sprite, Mr. Pibb, Root Beer and Orange"
-  description: "Small 16 oz"
-  price: 1.80
-  Image: http://goodfellasitalianrestaurant.com/wp-content/uploads/2014/10/soda_2liter_bottles.jpg
+  {name: "Beverages / Fresh Chilled Tamarindo, Horchata, Jamaica & Limonada",
+  description: "Large 32 oz",
+  price: "2.60",
+  Image: "https://s3-media4.fl.yelpcdn.com/bphoto/BfLofBDabsQXE4DRj7n_Rg/o.jpg"
   },
 
-  {name: "Carboneted Drinks / Coca Cola, Diet Coke, Sprite, Mr. Pibb, Root Beer and Orange"
-  description: "Medium 24 oz"
-  price: 1.95
-  Image: http://goodfellasitalianrestaurant.com/wp-content/uploads/2014/10/soda_2liter_bottles.jpg
+  {name: "Carboneted Drinks / Coca Cola, Diet Coke, Sprite, Mr. Pibb, Root Beer and Orange",
+  description: "Small 16 oz",
+  price: "1.80",
+  Image: "http://goodfellasitalianrestaurant.com/wp-content/uploads/2014/10/soda_2liter_bottles.jpg"
   },
 
-  {name: "Carboneted Drinks / Coca Cola, Diet Coke, Sprite, Mr. Pibb, Root Beer and Orange"
-  description: "Large 32 oz"
-  price: 2.30
-  Image: http://goodfellasitalianrestaurant.com/wp-content/uploads/2014/10/soda_2liter_bottles.jpg
+  {name: "Carboneted Drinks / Coca Cola, Diet Coke, Sprite, Mr. Pibb, Root Beer and Orange",
+  description: "Medium 24 oz",
+  price: "1.95",
+  Image: "http://goodfellasitalianrestaurant.com/wp-content/uploads/2014/10/soda_2liter_bottles.jpg"
   },
+
+  {name: "Carboneted Drinks / Coca Cola, Diet Coke, Sprite, Mr. Pibb, Root Beer and Orange",
+  description: "Large 32 oz",
+  price: "2.30",
+  Image: "http://goodfellasitalianrestaurant.com/wp-content/uploads/2014/10/soda_2liter_bottles.jpg"
+  }
+
 ]
